0bab0bd18544ebefae75d49b56f11ae2379c49a4
YlianaLisova <lisova229@gmail.com>
2022-05-16 15:15:54 +0300

    Angular lecture 2 ddos

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {UserService} from "../../services/user.service";
+import {IUserDetails} from "../../models/IUserDetails";
 
 @Component({
   selector: 'app-user-details',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-details.component.css']
 })
 export class UserDetailsComponent implements OnInit {
+  userDetails : IUserDetails;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute,private userService: UserService) { }
 
   ngOnInit(): void {
+    // this.activatedRoute.url.subscribe(value => console.log(value));
+    this.activatedRoute.params.subscribe(({id}) =>{
+      this.userService.getUser(+id).subscribe(value => this.userDetails = value as IUserDetails)
+    });
   }
 
 }
--- src/app/models/IUser.ts
+++ src/app/models/IUser.ts
@@ -3,6 +3,5 @@ import {ICompany} from "./ICompany";
 export interface IUser{
   id: number;
   name: string;
-  username: string;
   company: ICompany;
 }
