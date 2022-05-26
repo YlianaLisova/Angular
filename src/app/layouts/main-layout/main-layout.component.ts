8d61db2227cd30dc5bd0fc86e3956c929dc7e6ec
YlianaLisova <lisova229@gmail.com>
2022-05-26 15:16:46 +0300

    homework 4 loading

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ResolveEnd, ResolveStart, Router} from "@angular/router";
+import {filter, map} from "rxjs";
 
 @Component({
   selector: 'app-main-layout',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainLayoutComponent implements OnInit {
 
-  constructor() { }
+  loading = false
+
+  constructor(private router: Router) {
+  }
 
   ngOnInit(): void {
+    this.router.events.pipe(
+      filter(e => e instanceof ResolveStart || e instanceof ResolveEnd),
+      map(e => e instanceof ResolveStart)
+    ). subscribe(value => this.loading = value)
   }
 
 }
--- src/app/modules/user/services/resolvers/user.resolver.ts
+++ src/app/modules/user/services/resolvers/user.resolver.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
+import {ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot} from '@angular/router';
 import {Observable} from 'rxjs';
 import {IUser} from "../../interfaces";
 import {UserService} from "../user.service";
@@ -9,10 +9,15 @@ import {UserService} from "../user.service";
 })
 export class UserResolver implements Resolve<IUser> {
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private router: Router) {
   }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<IUser> | Promise<IUser> | IUser {
+    const user = this.router.getCurrentNavigation()?.extras?.state?.['user']
+
+    if (user) {
+      return user
+    }
     const {id} = route.params;
     return this.userService.getById(id)
   }
