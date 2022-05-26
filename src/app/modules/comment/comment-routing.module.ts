e34a12709c92f78e4cc84b3c999a8862ec9f3914
YlianaLisova <lisova229@gmail.com>
2022-05-26 16:57:07 +0300

    homework 4 guard

--- src/app/modules/comment/comment-routing.module.ts
+++ src/app/modules/comment/comment-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import {CommentsComponent} from "./components/comments/comments.component";
 import {CommentDetailsComponent} from "./components/comment-details/comment-details.component";
 import {CommentResolver} from "./services";
+import {CommentGuard} from "./guards";
 
 const routes: Routes = [{
-  path: '', component: CommentsComponent, children:[
-    {path: ':id', component: CommentDetailsComponent, resolve: {data: CommentResolver}}
+  path: '', component: CommentsComponent,canActivate:[CommentGuard], children:[
+    {path: ':id', component: CommentDetailsComponent,canActivate:[CommentGuard], resolve: {data: CommentResolver}}
   ]
 }];
 
--- src/app/modules/post/post-routing.module.ts
+++ src/app/modules/post/post-routing.module.ts
@@ -3,9 +3,10 @@ import {RouterModule, Routes} from '@angular/router';
 import {PostsComponent} from "./components/posts/posts.component";
 import {PostDetailComponent} from "./components/post-detail/post-detail.component";
 import {PostResolver} from "./services";
+import {PostGuard} from "./guards/post.guard";
 
 const routes: Routes = [{
-  path: '', component: PostsComponent, children: [
+  path: '', component: PostsComponent,canActivate:[PostGuard], children: [
     {path: ':id', component: PostDetailComponent, resolve: {data: PostResolver}}
   ]
 }];
