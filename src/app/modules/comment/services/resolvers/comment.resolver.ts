13680e1b02d6a4b7a663667743c90612bffdae0d
YlianaLisova <lisova229@gmail.com>
2022-05-26 14:54:02 +0300

    homework 4 resolver

--- src/app/modules/comment/services/resolvers/comment.resolver.ts
+++ src/app/modules/comment/services/resolvers/comment.resolver.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
+import {ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot} from '@angular/router';
 import {Observable} from 'rxjs';
 import {IComment} from "../../interfaces";
 import {CommentService} from "../comment.service";
@@ -8,10 +8,14 @@ import {CommentService} from "../comment.service";
   providedIn: 'root'
 })
 export class CommentResolver implements Resolve<IComment> {
-  constructor(private commentService: CommentService) {
+  constructor(private commentService: CommentService, private router: Router) {
   }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<IComment> | Promise<IComment> | IComment {
+    const comment = this.router.getCurrentNavigation()?.extras?.state?.['comment']
+    if (comment){
+      return comment
+    }
     const {id} = route.params;
     return this.commentService.getById(id);
   }
