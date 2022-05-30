fb75b1d8b9cfdd4cc844da075d628da4c4b4fc95
YlianaLisova <lisova229@gmail.com>
2022-05-31 01:40:49 +0300

    lecture 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {MainLayoutComponent} from './layouts/main-layout/main-layout.component';
+import {CarsComponent} from './components/cars/cars.component';
+import {CarComponent} from './components/car/car.component';
+import {RegisterComponent} from './components/register/register.component';
+import {LoginComponent} from './components/login/login.component';
+import {HeaderComponent} from './components/header/header.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {ReactiveFormsModule} from "@angular/forms";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {MainInterceptor} from "./main.interceptor";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    CarsComponent,
+    CarComponent,
+    RegisterComponent,
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    multi: true,
+    useClass: MainInterceptor
+  }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/main.interceptor.ts
@@ -0,0 +1,40 @@
+import {Injectable} from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor, HttpErrorResponse
+} from '@angular/common/http';
+import {catchError, Observable} from 'rxjs';
+import {AuthService} from "./services";
+import {Router} from "@angular/router";
+
+@Injectable()
+export class MainInterceptor implements HttpInterceptor {
+
+  constructor(private authService: AuthService, private router: Router) {
+  }
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const isAuthenticated = this.authService.isAuthorization();
+
+    if (isAuthenticated) {
+      request = this.addToken(request, this.authService.getToken())
+    }
+    return next.handle(request).pipe(
+    // @ts-ignore
+      catchError((res:HttpErrorResponse) => {
+        if (res && res.error && res.status === 401){
+          this.authService.deleteToken();
+          this.router.navigate(['login'])
+        }
+      })
+    );
+  }
+
+  addToken(request: HttpRequest<any>, token: string): HttpRequest<any> {
+    return request.clone({
+      setHeaders: {Authorization: `Bearer ${token}`}
+    })
+  }
+}
